3eeb6671aa697a3dd89b499dbb3240920354d137
Ilias El Hanouch <100472198@alumnos.uc3m.es>
2023-09-13 17:53:45 +0200

    BBDD

---
+++ src/main/database_managament/db_creations.py
@@ -0,0 +1,19 @@
+import sqlite3
+conn = sqlite3.connect('app_database.db')
+def create():
+    cursor = conn.cursor()
+    cursor.execute("""CREATE TABLE IF NOT EXISTS usuarios (
+                        NICKNAME VARCHAR(21) NOT NULL,
+                        PASSWORD VARCHAR(100) NOT NULL,	
+                        PRIMARY KEY(NICKNAME)
+                    );""")
+    cursor.execute("""CREATE TABLE IF NOT EXISTS user_info (
+                        USER VARCHAR(21),
+                        MONEY NUMERIC(10,2),
+                        EMAIL VARCHAR(50),
+                        NAME VARCHAR(30),
+                        SURNAME1 VARCHAR(50),
+                        SURNAME2 VARCHAR(50),
+                        PRIMARY KEY(USER),
+                        FOREIGN KEY(USER) references USUARIOS(NICKNAME)
+                    );""")
---
+++ src/main/database_managament/prueba_bbdd.py
@@ -0,0 +1,10 @@
+import db_management
+import db_creations
+db_creations.create()
+db_management.insert_new_user("Juan","pedrito1233")
+print(db_management.search_user("Juan"))
+print(db_management.get_acc_money("Juan"))
+
+
+
+
