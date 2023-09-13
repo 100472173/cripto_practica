2fb598b6956d1443bf0d54d8401dd2ddc5bf5650
100472173 <100472173@alumnos.uc3m.es>
2023-09-13 17:55:28 +0200

    interfaces comienzo

--- src/main/GUI/interface.py
+++ src/main/GUI/interface.py
@@ -6,7 +6,4 @@ class Interface(customtkinter.CTk):
     def __init__(self):
         super().__init__()
         self.geometry("500x500")
-        self.title("My Balance")
-
-
-
+        self.title("My Balance ")
