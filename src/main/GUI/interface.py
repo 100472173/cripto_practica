80c5c6c6f120c9437fb055439385bd44ad49dde6
Ilias El Hanouch <100472198@alumnos.uc3m.es>
2023-09-13 17:53:21 +0200

    BBDD

--- src/main/GUI/interface.py
+++ src/main/GUI/interface.py
@@ -5,6 +5,4 @@ app = tkinter.Tk()
 app.geometry("500x500")
 customtkinter.set_appearance_mode("dark")  # Modes: system (default), light, dark
 customtkinter.set_default_color_theme("dark-blue")
-
-
 app.mainloop()
--- src/main/database_managament/db_management.py
+++ src/main/database_managament/db_management.py
@@ -1,9 +1,22 @@
+import sqlite3
+conn = sqlite3.connect('app_database.db')
+def insert_new_user(username,pwd):
+    cursor = conn.cursor()
+    sql_query = f"INSERT INTO usuarios(nickname,password) values ('{username}','{pwd}');"
+    cursor.execute(sql_query)
 
-
-def insert_new_user():
-
-def search_user():
-
-def get_acc_money():
-
-def modify_money():
+def search_user(username: str) -> bool:
+    cursor = conn.cursor()
+    sql_query = f"SELECT * FROM usuarios WHERE nickname = '{username}';"
+    cursor.execute(sql_query)
+    info = cursor.fetchall()
+    return len(info) > 0
+def get_acc_money(username):
+    cursor = conn.cursor()
+    sql_query = f"SELECT money FROM user_info WHERE USER = '{username}';"
+    info= cursor.fetchall()
+    print(info)
+def modify_money(username,new_money):
+    cursor = conn.cursor()
+    money = str(new_money)
+    sql_statement = f"UPDATE user_info SET money = {money} WHERE nickname = '{username}';"
