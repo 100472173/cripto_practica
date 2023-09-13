c9cfc33810e11bfd639542c203c9bcc0e54a7320
Ilias El Hanouch <100472198@alumnos.uc3m.es>
2023-09-13 18:51:16 +0200

    mas cosas

---
+++ src/main/user_authentication/pruebas_user_auth.py
@@ -0,0 +1,12 @@
+import user_auth
+import re
+from database_management import db_management
+from database_management import db_creations
+db_creations.create()
+username = "12j4"
+print(user_auth.check_username_syntax(username))
+
+validation_pattern = r"^(?=.*[A-Z])(?=.*\d)(?=.*[~@_/:+]).{8,}$"
+myreg = re.compile(validation_pattern)
+print(myreg.fullmatch("Alo123!!"))
+db_management.insert_new_user("Juan","lelele")
--- src/main/user_authentication/user_auth.py
+++ src/main/user_authentication/user_auth.py
@@ -1,11 +1,18 @@
-#
-
+from database_management import db_management
+import re
 def check_username_syntax(username: str):
-    """Comprueba que el nombre de usuario sigue el patrón definido como correcto
-       El patrón sigue estas reglas:
-          - Debe contener al menos una letra
-          - La longitud debe estar entre 4-20 caracteres """
-
+    """ Comprueba que el nombre de usuario sigue el patrón definido como correcto
+        El patrón sigue estas reglas:
+        - Debe contener al menos una letra
+        - La longitud debe estar entre 4-20 caracteres
+        """
+    validation_pattern = r"^(?=.*[a-zA-Z]).{4,20}$"
+    my_regex = re.compile(validation_pattern)
+    result = my_regex.fullmatch(username)
+    if not result:
+        print("Wrong pattern")
+    else:
+        print("OK.")
 
 def check_pwd_syntax(pwd: str):
     """Comprueba que la contraseña sigue el patrón definido como correcto
@@ -15,8 +22,5 @@ def check_pwd_syntax(pwd: str):
               - Al menos un caracter especial (~@_/+:)
               - Longitud de al menos 8 caracteres"""
 
-
-def check_user(username: str, pwd: str):
-    """Comprueba que el usuario está en la base de datos"""
 
 
