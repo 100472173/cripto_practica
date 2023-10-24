f1c183ff6d12778d0fb02c22e1f33a536dc088e9
100472198 <100472198@alumnos.uc3m.es>
2023-10-24 19:25:20 +0200

    Ultimas configuraciones proyecto

--- src/main/GUI/main.py
+++ src/main/GUI/main.py
@@ -1,6 +1,6 @@
 from interface import Interface
 from database_management import db_creations
-
-db_creations.create()
-app = Interface()
-app.mainloop()
+if __name__ == "__main__":
+    db_creations.create()
+    app = Interface()
+    app.mainloop()
