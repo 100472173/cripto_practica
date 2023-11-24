fc753b9ec08b105898a8bc3bc3f99d84ec866b64
ili <ilias.el@alumnos.uc3m.es>
2023-11-24 12:08:40 +0100

    Creacion de funcion que escribe en fichero

--- src/main/database_management/assymetric_management.py
+++ src/main/database_management/assymetric_management.py
@@ -1,1 +1,14 @@
-def write_file(username, private_key, public_key):
+import os
+
+def write_file(username, key1: bytes):
+    try:
+        directorio_actual = os.path.dirname(os.path.abspath(__file__))
+        ruta_absoluta = os.path.join(directorio_actual, '..', 'user_files', "files.txt")
+        with open(ruta_absoluta, 'a') as archivo:
+            archivo.write(f"{username}:{key1};\n")
+        print(f"Se ha escrito en el archivo: {ruta_absoluta}")
+    except Exception as e:
+        print(f"Error al escribir en el archivo: {e}")
+
+# Ejemplo de depuracion
+write_file("Jose", bytes("12345", encoding="UTF-8"))
